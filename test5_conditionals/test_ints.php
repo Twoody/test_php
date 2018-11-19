8a08402b771a3efe00ee54825c83d030c80c50c8
Tanner Lee Woody <tannerleewoody@Tanners-MacBook-Air.local>
2018-11-18 23:05:13 -0800

    End of conditional playing for test5; Init for dummy repo;
    Will copy testXXX dir content to new test dirs as we move along;

--- test5_conditionals/test_ints.php
+++ test5_conditionals/test_ints.php
@@ -1,7 +1,47 @@
 <?php
 	require('./../utils/html.php');
 
-	$myString = makeP("Helloworld (4th time? really?),");
-	$myString .= makeP(tab()."This test is going to involve integers and math");
+	$myString = makeP("Helloworld,");
+	$myString .= makeP(tab()."This test is going to involve booleans and conditionals");
+
+	$a = 55;
+	$b = "A string";
+	if ($a > $b){
+		$myString .= makeP("Integers are more than strings.");
+	}
+	else{
+		$myString .= makeP("Integers are more than strings.");
+	}
+	$myString .= makeP((string)($a>$b));
+	$myString .= makeP((string)($a===$b));
+
+	if ($a > $b && false){
+		$myString .= makeP("This should never happen...");
+	}
+	else
+		$myString .= makeP("False makes everything false");
+
+	if ($a > $b && false==false)
+		$myString .= makeP("Unless false == false");
+	else
+		$myString .= makeP("This should never happen...");
+
+	if (true == "foo")
+		$myString .= makeP("true == 'foo'");
+	if(true === "foo")
+		$myString .= makeP("true === 'foo'");
+	if(true != "foo")
+		$myString .= makeP("true != 'foo'");
+	if(true !== "foo")
+		$myString .= makeP("true !== 'foo'");
+	
+	if (true || false)
+		$myString .= makeP("true || false");
+		
+	if ("foo" || false)
+		$myString .= makeP("'foo' || false");
+	
+	$myString .= (true == true ? $myString.="Even ternary!?" : $myString.="Or not?");
+
 	echo $myString;
 ?>
